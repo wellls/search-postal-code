9e803761b296b06f50d2e018deed33e6482e9c67
Wellington Lima <wellington.map@hotmail.com>
2024-07-10 09:15:46 -0300

    feat: search postal code and write city file

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
 func main() {
-	println("Hello World")
+	for _, cep := range os.Args[1:] {
+		req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error making request: %v\n", err)
+		}
+		defer req.Body.Close()
+
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading response: %v\n", err)
+		}
+
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing response: %v\n", err)
+		}
+
+		file, err := os.Create("city.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating file: %v\n", err)
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
+		}
+
+		fmt.Println("Cidade: ", data.Localidade)
+	}
 }
